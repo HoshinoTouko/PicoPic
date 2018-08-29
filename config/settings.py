52106fad2b5cc01bebbe5b1f289e5bc56768a25b
HoshinoTouko <oranly@hotmail.com>
2018-08-29 12:42:06 +0800

    Path modified.

--- config/settings.py
+++ config/settings.py
@@ -56,7 +56,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'PicoPic.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -74,7 +74,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'PicoPic.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
