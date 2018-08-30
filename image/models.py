e0b119aa1dcc575150e270ed4a2b47e2bf4d4651
HoshinoTouko <oranly@hotmail.com>
2018-08-30 12:14:59 +0800

    Init image folder.

--- image/models.py
+++ image/models.py
@@ -1,11 +1,19 @@
+'''
+File: models.py
+Created Date: Wednesday August 29th 2018
+Author: Touko Hoshino
+-----
+Last Modified:
+Modified By:
+-----
+Copyright (c) 2018 Your Company
+'''
 from django.db import models
 
 from post.models import Post
 
 
 class Image(models.Model):
+    """Image module"""
     name = models.CharField(unique=True, max_length=50)
     post = models.ForeignKey(Post, related_name='image', on_delete=models.CASCADE)
-
-    def __str__(self): 
-        return self.name
---
+++ image/services.py
@@ -0,0 +1,10 @@
+'''
+File: services.py
+Created Date: Thursday August 30th 2018
+Author: Touko Hoshino
+-----
+Last Modified:
+Modified By:
+-----
+Copyright (c) 2018 Your Company
+'''
